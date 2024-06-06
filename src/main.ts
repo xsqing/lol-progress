b8c76ff7230a561d4db1f8adea73511fb39f2125
xuesiqing <xuesiqing@cmss.chinamobile.com>
2024-06-06 19:41:40 +0800

    first commit

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import './assets/main.css'
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import '@google/model-viewer'
+const app = createApp(App)
+
+app.mount('#app')
---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue({
+      template: {
+        // 添加以下内容
+        compilerOptions: {
+          isCustomElement: (tag) => ['model-viewer'].includes(tag)
+        }
+      }
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  assetsInclude: ['**/*.glb']
+})
